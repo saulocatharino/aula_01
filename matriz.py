cf5f9fd7a12bad9c536560f7fa8edaa4f7ea7618
Saulo Catharino <saulocatharino@gmail.com>
2018-07-22 11:25:05 -0300

    Add files via upload

---
+++ matriz.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+Y = []
+
+for x in range(3):
+   for y in range(30):
+       Y.append([[255,0,0],[0,255,0],[0,0,255]])
+
+YYY = np.array(Y)
+YY = YYY.reshape(-1,30,3)
+
+plt.imshow(YY)
+cv2.imwrite("teste.png",YY)
+plt.show()
